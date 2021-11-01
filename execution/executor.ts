632218e05d33e3f08efa9b102b826c4115a16461
GitHub Action Script <please@open.issue>
2021-11-01 16:26:56 +0000

    Deploy 14742f61da387dc3db35b54642507277213d7c46 to 'deno' branch

--- execution/executor.ts
+++ execution/executor.ts
@@ -50,6 +50,7 @@ import {
   getArgumentValues,
   getDirectiveValues,
 } from './values.ts';
+import type { PatchFields } from './collectFields.ts';
 import {
   collectFields,
   collectSubfields as _collectSubfields,
@@ -480,6 +481,7 @@ export class Executor {
       operation,
       variableValues,
       disableIncremental,
+      errors,
     } = exeContext; // TODO: replace getOperationRootType with schema.getRootType
 
     const rootType = getOperationRootType(schema, operation);
@@ -511,7 +513,7 @@ export class Executor {
           rootValue,
           path,
           fields,
-          exeContext.errors,
+          errors,
         );
         break;
 
@@ -533,29 +535,11 @@ export class Executor {
           rootValue,
           path,
           fields,
-          exeContext.errors,
+          errors,
         );
     }
 
-    for (const patch of patches) {
-      const { label, fields: patchFields } = patch;
-      const errors: Array<GraphQLError> = [];
-      this.addFields(
-        exeContext,
-        this.executeFields(
-          exeContext,
-          rootType,
-          rootValue,
-          path,
-          patchFields,
-          errors,
-        ),
-        errors,
-        label,
-        path,
-      );
-    }
-
+    this.executePatches(exeContext, patches, rootType, rootValue, path);
     return result;
   }
   /**
@@ -1374,26 +1358,7 @@ export class Executor {
       subFieldNodes,
       errors,
     );
-
-    for (const subPatch of subPatches) {
-      const { label, fields: subPatchFieldNodes } = subPatch;
-      const subPatchErrors: Array<GraphQLError> = [];
-      this.addFields(
-        exeContext,
-        this.executeFields(
-          exeContext,
-          returnType,
-          result,
-          path,
-          subPatchFieldNodes,
-          subPatchErrors,
-        ),
-        subPatchErrors,
-        label,
-        path,
-      );
-    }
-
+    this.executePatches(exeContext, subPatches, returnType, result, path);
     return subFields;
   }
   /**
@@ -1584,6 +1549,34 @@ export class Executor {
 
   hasSubsequentPayloads(exeContext: ExecutionContext) {
     return exeContext.subsequentPayloads.length !== 0;
+  }
+
+  executePatches(
+    exeContext: ExecutionContext,
+    patches: Array<PatchFields>,
+    parentType: GraphQLObjectType,
+    source: unknown,
+    path: Path | undefined,
+    errors?: Maybe<Array<GraphQLError>>,
+  ): void {
+    for (const patch of patches) {
+      const { label, fields: patchFields } = patch;
+      const patchErrors = errors ?? [];
+      this.addFields(
+        exeContext,
+        this.executeFields(
+          exeContext,
+          parentType,
+          source,
+          path,
+          patchFields,
+          patchErrors,
+        ),
+        patchErrors,
+        label,
+        path,
+      );
+    }
   }
 
   addFields(
