45672e73034bb6e55a1d764d6f07c8be014eb838
GitHub Action Script <please@open.issue>
2021-11-22 22:01:15 +0000

    Deploy 9c40055b1a8af746baddafb113b2dd2e6e3126ba to 'deno' branch

--- execution/executor.ts
+++ execution/executor.ts
@@ -102,7 +102,7 @@ interface ExecutionContext {
   resolveField: FieldResolver;
   errors: Array<GraphQLError>;
   subsequentPayloads: Array<DispatcherResult>;
-  iterators: Array<AsyncIterator<unknown>>;
+  iterators: Set<AsyncIterator<unknown>>;
   publisher:
     | {
         push: Push<ExecutionPatchResult>;
@@ -393,7 +393,9 @@ export class Executor {
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
         stop.then(() =>
           Promise.all(
-            exeContext.iterators.map((iterator) => iterator.return?.()),
+            Array.from(exeContext.iterators.values()).map((iterator) =>
+              iterator.return?.(),
+            ),
           ),
         );
         exeContext.publisher = {
@@ -559,7 +561,7 @@ export class Executor {
           : this.buildFieldResolver('resolve', defaultResolveFieldValueFn),
       errors: [],
       subsequentPayloads: [],
-      iterators: [],
+      iterators: new Set(),
       publisher: undefined,
       pendingPushes: 0,
     };
@@ -583,7 +585,7 @@ export class Executor {
       ),
       errors: [],
       subsequentPayloads: [],
-      iterators: [],
+      iterators: new Set(),
       publisher: undefined,
       pendingPushes: 0,
     };
@@ -1754,7 +1756,7 @@ export class Executor {
     label?: string,
   ): Promise<void> {
     const { iterators } = exeContext;
-    iterators.push(iterator);
+    iterators.add(iterator);
     let index = initialIndex;
     let iteration = await this.advanceAsyncIterator(
       index,
@@ -1835,7 +1837,7 @@ export class Executor {
     iterator: AsyncIterator<unknown>,
   ): void {
     const { iterators, publisher } = exeContext;
-    iterators.splice(iterators.indexOf(iterator), 1);
+    iterators.delete(iterator);
 
     if (!this.hasNext(exeContext) && publisher) {
       const { push, stop } = publisher; // eslint-disable-next-line @typescript-eslint/no-floating-promises
@@ -1848,7 +1850,7 @@ export class Executor {
   }
 
   hasNext(exeContext: ExecutionContext): boolean {
-    return exeContext.pendingPushes > 0 || exeContext.iterators.length > 0;
+    return exeContext.pendingPushes > 0 || exeContext.iterators.size > 0;
   }
 
   queue(
