6035699a11c6c4bef3b1d88f1f05b77e0b963866
GitHub Action Script <please@open.issue>
2021-11-22 20:51:51 +0000

    Deploy f8a5ac1f50f32029bc5e47dc6941b0cdabd5dfbe to 'deno' branch

--- execution/executor.ts
+++ execution/executor.ts
@@ -1793,9 +1793,19 @@ export class Executor {
 
             if (isPromise(completedItem)) {
               // eslint-disable-next-line @typescript-eslint/no-floating-promises
-              completedItem.then((resolvedItem) =>
-                this.queue(exeContext, resolvedItem, errors, itemPath, label),
-              );
+              completedItem // Note: we don't rely on a `catch` method, but we do expect "thenable"
+                // to take a second callback for the error case.
+                .then(undefined, (rawError) => {
+                  const error = locatedError(
+                    rawError,
+                    fieldNodes,
+                    pathToArray(itemPath),
+                  );
+                  return this.handleFieldError(error, itemType, errors);
+                })
+                .then((resolvedItem) =>
+                  this.queue(exeContext, resolvedItem, errors, itemPath, label),
+                );
               return;
             }
 
