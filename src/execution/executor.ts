d3016ce14567b67dde7357e6076a1b3fb3c2d3ca
Yaacov Rydzinski <yaacovCR@gmail.com>
2021-12-07 05:25:24 +0200

    remove unnecessary method (#107)
    * remove unnecessary method
    
    fold handleIncrementalRawError into advanceAsyncIterator
    
    * prettier

--- src/execution/executor.ts
+++ src/execution/executor.ts
@@ -1970,30 +1970,16 @@ export class Executor {
     } catch (rawError) {
       exeContext.pendingPushes++;
       const itemPath = addPath(path, index, undefined);
-      this.handleIncrementalRawError(
+      const error = locatedError(rawError, fieldNodes, pathToArray(itemPath));
+      this.handleFieldError(error, itemType, payloadContext.errors);
+      this.queue(
         exeContext,
-        rawError,
-        fieldNodes,
-        itemType,
-        itemPath,
         payloadContext,
         prevPayloadContext,
+        null,
+        itemPath,
       );
     }
-  }
-
-  handleIncrementalRawError(
-    exeContext: ExecutionContext,
-    rawError: unknown,
-    fieldNodes: ReadonlyArray<FieldNode>,
-    type: GraphQLOutputType,
-    path: Path,
-    payloadContext: PayloadContext,
-    prevPayloadContext: PayloadContext,
-  ): void {
-    const error = locatedError(rawError, fieldNodes, pathToArray(path));
-    this.handleFieldError(error, type, payloadContext.errors);
-    this.queue(exeContext, payloadContext, prevPayloadContext, null, path);
   }
 
   closeAsyncIterator(
