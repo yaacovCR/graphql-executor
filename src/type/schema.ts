d06133c6da69458139ce3d8694112ec416ac2a48
Yaacov Rydzinski <yaacovCR@gmail.com>
2021-12-15 21:44:04 +0200

    Do not validate schema on first execution (#112)
    * Do not validate schema on first execution
    
    Schemas can still be validated when desired using the dedicated validateSchema, which should usually be done as a separate step to avoid unnecessary revalidation.
    
    Previously, by default, on first execution, a schema would be validated (if not validated already). This behavior could be disabled by using the assumeValid option with schema creation.
    
    * add changeset

---
+++ src/type/schema.ts
@@ -0,0 +1,11 @@
+import type { GraphQLSchema } from 'graphql';
+import { isSchema } from 'graphql';
+
+import { inspect } from '../jsutils/inspect';
+
+export function assertSchema(schema: GraphQLSchema): GraphQLSchema {
+  if (!isSchema(schema)) {
+    throw new Error(`Expected ${inspect(schema)} to be a GraphQL schema.`);
+  }
+  return schema;
+}
